446b76456fab41435995a02178e6f1494ca76248
Allison Rodrigues <allison_rps@hotmail.com>
2024-10-23 12:09:38 -0300

    Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ dotenv.config()
 import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-
+import cors from 'cors';
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 
@@ -19,7 +19,7 @@ app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(cors());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
