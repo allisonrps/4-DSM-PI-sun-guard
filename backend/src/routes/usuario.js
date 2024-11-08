7ee288de595c610d1e76421d2b8b79e9fec34cc8
Allison Rodrigues <allison_rps@hotmail.com>
2024-11-08 16:13:08 -0300

    Rota Login

--- backend/src/routes/usuario.js
+++ backend/src/routes/usuario.js
@@ -8,5 +8,7 @@ router.get('/', controller.retrieveAll)
 router.get('/:id', controller.retrieveOne)
 router.put('/:id', controller.update)
 router.delete('/:id', controller.delete)
+// Rota de login
+router.post('/login', controller.login);
 
 export default router
